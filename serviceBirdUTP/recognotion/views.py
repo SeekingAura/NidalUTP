dca8d374dda86da8cee94315046107a6b7739a0d
Carlos Arturo <carlox216@utp.edu.co>
2018-11-08 19:29:45 -0500

    set get values of GET and POST (service)

--- serviceBirdUTP/recognotion/views.py
+++ serviceBirdUTP/recognotion/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
-
+from django.views.decorators.csrf import csrf_exempt
 import warnings
 warnings.filterwarnings("ignore")
 import sys
@@ -21,16 +21,54 @@ config = {
 	"db": "avesscheme"
 }
 }
-
+@csrf_exempt
 def useRecognotion(request):
 	if(request.method == 'GET'):
-		print("hey se hizo GET")
+		print("hey se hizo GET", request.GET)
+		for key in request.GET:
+			print(key)
+			value = request.GET[key]
+			print(value)
 		#print(request.REQUEST['section'])
 		# request.method.get.POST("")
+		"""
+		# create a Dejavu instance
+		djv = Dejavu(config)
+
+		# Fingerprint all the mp3's in the directory we give it
+		# djv.fingerprint_directory("mp3 prueba", [".mp3"]
+
+
+		# Recognize audio from a file
+		results = djv.recognize(FileRecognizer, sys.argv[1])
+		confidenceOverall=0
+		for result in results:
+			confidenceOverall+=result.get("confidence")
+
+
+		resultsOverall={}
+
+		## only name of birds, production mode
+		for result in results:
+			if(result.get("song_name").split("-")[0] in resultsOverall):
+				resultsOverall[result.get("song_name").split("-")[0]]+=result.get("confidence")
+			else:
+				resultsOverall[result.get("song_name").split("-")[0]]=result.get("confidence")
+		
+		print("data ->", resultsOverall)
+		results=[]
+		for result in sorted(resultsOverall.items(), key=lambda kv: kv[1], reverse=True):#order by value (number of confidences)
+			print("ave {} se estima ser {:0.2f}%".format(result[0], (result[1]/confidenceOverall)*100))
+			results.append([result[0], "{:0.2f}%".format((result[1]/confidenceOverall)*100)])
+		"""
 
 	if(request.method == 'POST'):
 		print("hey se hizo post")
+		for key in request.POST:
+			print(key)
+			value = request.POST[key]
+			print(value)
 		# request.method.get.POST("")
-	return JsonResponse({'mystring':"[['MelanerpesFormicivorus', '74.56%'], ['AramidesCajaneus', '8.77%'], ['SynallaxisBrachyura', '7.02%'], ['RupornisMagnirostris', '3.07%'], ['LepidocolaptesSouleyetii', '2.19%'], ['PheugopediusSclateri', '1.75%'], ['SynallaxisAzarae', '1.32%'], ['XiphorhynchusSusurrans', '0.88%'], ['DryocopusLineatus', '0.44%']]"})
+	return JsonResponse({'lista':"[['MelanerpesFormicivorus', '74.56%'], ['AramidesCajaneus', '8.77%'], ['SynallaxisBrachyura', '7.02%'], ['RupornisMagnirostris', '3.07%'], ['LepidocolaptesSouleyetii', '2.19%'], ['PheugopediusSclateri', '1.75%'], ['SynallaxisAzarae', '1.32%'], ['XiphorhynchusSusurrans', '0.88%'], ['DryocopusLineatus', '0.44%']]"})
 # Create your views here.
 
